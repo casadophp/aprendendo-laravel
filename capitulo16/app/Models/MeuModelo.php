323871fc85532295c74cb00ce6851553e02fb3cb
marabesi <matheus.marabesi@gmail.com>
2016-07-30 21:59:43 +0000

    Novos exemplos criados do capÃtulo 14 e 16

---
+++ capitulo16/app/Models/MeuModelo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MeuModelo extends Model
+{
+    protected $table = 'nome_da_tabela';
+    protected $primaryKey = 'nome_da_coluna';
+}
---
+++ capitulo16/database/migrations/2016_07_30_155923_tabela_usuarios.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TabelaUsuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \DB::statement('
+            CREATE TABLE IF NOT EXISTS `usuarios` (
+            `id` INT NOT NULL AUTO_INCREMENT COMMENT \'	\',
+            `nome` VARCHAR(45) NOT NULL,
+            `email` VARCHAR(200) NOT NULL,
+            `senha` VARCHAR(255) NOT NULL,
+            PRIMARY KEY (`id`))
+            ENGINE = InnoDB
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        \DB::statement('DROP TABLE `usuarios`;');
+    }
+}
