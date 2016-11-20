8cdaeb0fa7a83c3d6db4fc91506390bc7f5a3e28
marabesi <matheus.marabesi@gmail.com>
2016-11-20 21:01:08 -0200

    finalizado exemplo 1 do capítulo 13

--- capitulo13/app/Http/Controllers/EmailSimplesController.php
+++ capitulo13/app/Http/Controllers/EmailSimplesController.php
@@ -12,11 +12,12 @@ class EmailSimplesController extends Controller
 			$assunto = \Request::input('assunto');
 			$mensagem = \Request::input('mensagem');	
 
-
 			\Mail::raw($mensagem, function($swiftMessage) use ($assunto) {
 				$swiftMessage->subject($assunto);
 				$swiftMessage->to('matheus.marabesi@gmail.com');	
 			});
+
+			return 'E-mail enviado com sucesso!';
 		}
 		
 		return view('email_simples');
