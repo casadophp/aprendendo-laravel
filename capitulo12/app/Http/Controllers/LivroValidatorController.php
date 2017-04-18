ae5dbecca1a4f71d398891d174d0500d02dc7884
marabesi <matheus.marabesi@gmail.com>
2017-04-18 20:13:09 -0300

    adicionado novos exemplos do capítulo12

--- capitulo12/app/Http/Controllers/LivroValidatorController.php
+++ capitulo12/app/Http/Controllers/LivroValidatorController.php
@@ -2,21 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Livro;
 use Illuminate\Http\Request;
 
-class LivroController extends Controller
+class LivroValidatorController extends Controller
 {
-
     public function salvar(Request $request)
     {
-        if ($request->input('titulo') == '') {
-            return 'Título não pode ser vazio';
-        }
-
-        if ($request->input('autor') == '') {
-            return 'Autor não pode ser vazio';
-        }
+        $this->validate($request, [
+            'titulo' => 'required',
+            'autor' => 'required',
+        ]);
 
         if (strlen($request->input('autor')) < 3) {
             return 'Autor deve possuir no mínimo 3 letras';
