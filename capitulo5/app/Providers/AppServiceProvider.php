56efe45ec182f10862f002375f31f25cabea18f6
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2017-04-29 19:17:43 -0300

    Insercao do capitulo 5

---
+++ capitulo5/app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Validator::extend('telefonefixo', function ($attribute, $value, $parameters, $validator) {
+            return preg_match('/(\(?\d{2}\)?) ?9?\d{4}-?\d{4}/', $value);
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
