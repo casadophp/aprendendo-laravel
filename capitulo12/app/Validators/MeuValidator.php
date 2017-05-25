da04c8f5cf959cf7720a8bdf52a7d52bde8ffe63
marabesi <matheus.marabesi@gmail.com>
2017-05-24 22:13:35 -0300

    adicionado novo diretório de validators

---
+++ capitulo12/app/Validators/MeuValidator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Validators;
+
+use Illuminate\Validation\Validator;
+
+class MeuValidator extends Validator
+{
+
+    public function validateMinhaRegraDeValidacao()
+    {
+
+    }
+}
