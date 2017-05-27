73944e1e9dfedafd2def7f239852935b21452f42
marabesi <matheus.marabesi@gmail.com>
2017-05-27 15:57:24 -0300

    corrigido nome do arquivo de migração

--- capitulo15/database/migrations/2016_10_30_215539_tabela_livro.php
+++ capitulo15/database/migrations/2016_10_30_215539_tabela_livro.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Pessoa extends Migration
+class TabelaLivro extends Migration
 {
     /**
      * Run the migrations.
