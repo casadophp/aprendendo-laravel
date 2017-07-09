6eed61967852831c7b055265ed9e4a7cd5ec4838
marabesi <matheus.marabesi@gmail.com>
2017-07-09 20:38:42 -0300

    adicionado arquivo de alyout do capítulo 13

---
+++ capitulo13/resources/views/email_layout.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style type="text/css">
+            .container {
+                width: 500px;
+            }
+            .promocao {
+                height: 150px;
+            }
+            .produto {
+                height: 200px;
+            }
+            .cor-fundo {
+                background-color: #03A9F4;
+            }
+            table {
+                border: none;
+                text-align: center;
+                text-transform: uppercase;
+                color: #FFFFFF;
+                font-weight: bold;
+                border-spacing: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <table class="container">
+            <thead>
+                <tr>
+                    <th colspan="2" class="promocao cor-fundo">promoção</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="produto cor-fundo">produto 1</td>
+                    <td class="cor-fundo">produto 2</td>
+                </tr>
+                <tr>
+                    <td class="produto cor-fundo">produto 3</td>
+                    <td class="cor-fundo">produto 4</td>
+                </tr>
+                <tr>
+                    <td class="produto cor-fundo">produto 5</td>
+                    <td class="cor-fundo">produto 6</td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
